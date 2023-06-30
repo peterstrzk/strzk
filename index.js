3e0d145a321eb0ebfc8b7dfb6e41f7347fa95805
Piotr Starzak <peterstrzk@gmail.com>
2023-06-30 02:08:51 +0200

    stronka

---
+++ index.js
@@ -0,0 +1,23 @@
+const rickShow = document.getElementById("rick");
+const showRickBtn = document.getElementById("showRickBtn")
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// showRickBtn.addEventListener('click', function() {
+//     // Usuń klasę 'hidden', aby odkryć element
+//     // rickShow.classList.remove('showR');
+//     rickShow.style.display = 'block';
+//     showRickBtn.remove();
+// });
+
+const showingRick = () => {
+    rickShow.style.display = 'block';
+    showRickBtn.remove();
+    player.requestFullscreen();
+    player.play()
+}
+
+showRickBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    showingRick();
+})
